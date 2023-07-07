816c52b591fc0e834326c9a79ce2eb25cd49a82f
three-halves <threehalves1@gmail.com>
2023-07-07 18:04:20 -0500

    main menu work; about button and window

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -2,9 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] GameObject mainWindow;
+    [SerializeField] GameObject aboutWindow;
+    [SerializeField] GameObject playButton;
+    [SerializeField] GameObject aboutButton;
+    [SerializeField] GameObject aboutCloseButton;
+
     public void SelectStart()
     {
         SceneManager.LoadScene("Battle");
@@ -12,6 +19,19 @@ public class MainMenu : MonoBehaviour
 
     public void SelectAbout()
     {
-        // TODO implement
+        ToggleWindows();
+        EventSystem.current.SetSelectedGameObject(aboutCloseButton);
+    }
+
+    public void CloseAbout()
+    {
+        ToggleWindows();
+        EventSystem.current.SetSelectedGameObject(aboutButton);
+    }
+
+    public void ToggleWindows()
+    {
+        mainWindow.SetActive(!mainWindow.activeSelf);
+        aboutWindow.SetActive(!aboutWindow.activeSelf);
     }
 }
