a73b8e722299c2dfbbd5c7da94e48fe76be3843d
three-halves <threehalves1@gmail.com>
2023-07-08 22:07:11 -0500

    implement fast forward

---
+++ Assets/FastForward.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FastForward : MonoBehaviour
+{
+    [SerializeField] Image image;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Time.timeScale = 2f;
+            image.enabled = true;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            image.enabled = false;
+        }
+    }
+}
