fd75d9dce6d320cbb499e9dd3ee1103996bcc9b5
three-halves <threehalves1@gmail.com>
2023-07-09 03:08:26 -0500

    ai cant use statswap

--- Assets/Scripts/Battler.cs
+++ Assets/Scripts/Battler.cs
@@ -121,7 +121,7 @@ public class Battler : MonoBehaviour
         List<int> valid = new List<int>();
         for (int i = 0; i < moveUsesRemaining.Length; i++)
         {
-            if (moveUsesRemaining[i] > 0) valid.Add(i);
+            if (moveUsesRemaining[i] > 0 && !moves[i].displayName.Equals("Status Swap")) valid.Add(i);
         }
 
         return valid;
