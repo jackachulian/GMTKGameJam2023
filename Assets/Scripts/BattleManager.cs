fde56b141a84253e0b24183295189287e16dfefa
three-halves <threehalves1@gmail.com>
2023-07-09 03:42:47 -0500

    balance and bug fixes

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -108,6 +108,7 @@ public class BattleManager : MonoBehaviour
         foreach (Battler battler in battlers)
         {
             // battler.ResetMoveUses();
+            battler.statusEffects = new List<StatusEffect>();
         }
     }
 
